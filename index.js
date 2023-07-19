6efebd2f9520b62a01b197d55a4d3c9724b02545
LuisC92 <luis.clara@wildcodeschool.com>
2023-07-19 08:05:47 +0100

    solution for 1st labour

--- index.js
+++ index.js
@@ -1,3 +1,60 @@
 // First Labour : Heracles vs Nemean Lion
 
 // use your Fighter class here
+const Fighter = require("./src/Fighter.js")
+
+// 🧔 Heracles, strength 20, dexterity 6
+const hercules = new Fighter("🧔 Hercules", 20, 6)
+
+// 🦁 Nemean Lion, strength 11, dexterity 13
+const nemean = new Fighter("🦁 Nemean Lion", 20, 6)
+
+function roundDisplay(fighter1, fighter2){
+    return `${fighter1.name} 🗡️ ${fighter2.name} 💙 ${fighter2.name}: ${fighter2.life}`
+}
+
+function score(fighter1,fighter2){
+    // return fighter1.isAlive() ? 
+    // {
+    //     winner: fighter1,
+    //     looser: fighter2
+    // } : {
+    //     winner: fighter2,
+    //     looser: fighter1
+    // }
+
+    if(fighter1.isAlive()){ // fighter 1 life > 0
+        return {
+            winner: fighter1,
+            looser: fighter2 
+        }
+    } else {
+        return {
+            winner: fighter2,
+            looser: fighter1 
+        }
+    }
+
+
+
+}
+
+
+let round = 1
+
+while(hercules.isAlive() && nemean.isAlive()){
+    console.log(`🕛 Round #${round}`)
+
+    hercules.fight(nemean)
+    console.log(roundDisplay(hercules, nemean));
+
+    nemean.fight(hercules)
+    console.log(roundDisplay(nemean, hercules));
+
+    round ++
+}
+
+const finalResult = score(hercules, nemean)
+
+console.log(`💀 ${finalResult.looser.name} is dead`);
+console.log(`🏆 ${finalResult.winner.name} wins (💙 ${finalResult.winner.life} )`);
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,36 @@
 /* Fighter class definition */
+
+const MAX_LIFE = 100
+
+class Fighter {
+    constructor(name, strength, dexterity){
+        this.name = name;
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.life = MAX_LIFE // 100 for all fighters
+    }
+
+    //* method to generate a random value between 1 and max
+    getRandomInt(maxValue){
+        return 1 + Math.floor(Math.random() * maxValue);
+    }
+
+    //* method to launch the attack
+    fight(defender){
+        const attackPoints = this.getRandomInt(this.strength)
+
+        const damage = Math.max(attackPoints - defender.dexterity, 0)
+
+        defender.life = Math.max(defender.life - damage, 0)
+
+    }
+
+    // check if the fighter is alive (life > 0)
+    isAlive (){
+        return this.life > 0
+    }
+
+}
+
+
+module.exports = Fighter
